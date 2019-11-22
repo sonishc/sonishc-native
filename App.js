7b8d07997eb7dff7912ff6437182979d0df8528e
Serhii Onishchuk <onixsonic@gmail.com>
2019-11-22 09:54:39 +0200

    Init comment

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'white'}}>Що ти там Pомка? Коли виходиш?</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
